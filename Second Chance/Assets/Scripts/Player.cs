4a6a466630fd09e596db54d6cddcd7d4f59fad63
Matteus Fernandes <83843532+matteusfernandes@users.noreply.github.com>
2022-10-18 13:16:11 -0300

    player: create a player basic move

--- Second Chance/Assets/Scripts/Player.cs
+++ Second Chance/Assets/Scripts/Player.cs
@@ -4,15 +4,32 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    [SerializeField] private float speed;
+
+    private Rigidbody2D rig;
+    private Vector2 _direction;
+
+    private void Start() {
+        rig = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    private void Update() {
+        OnInput();
     }
+
+    private void FixedUpdate() {
+        OnMove();
+    }
+
+    #region Movement
+    
+    void OnInput() {
+        _direction = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+    }
+
+    void OnMove() {
+        rig.MovePosition(rig.position + _direction * speed * Time.fixedDeltaTime);
+    }
+
+    #endregion
 }
