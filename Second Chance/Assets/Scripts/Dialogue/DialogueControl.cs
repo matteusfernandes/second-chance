29b13b650de50f77ea7494327ec4d1d2a0d6d3e9
Matteus Fernandes <83843532+matteusfernandes@users.noreply.github.com>
2022-10-18 21:08:28 -0300

    feat: create npc system dialogue and hud

---
+++ Second Chance/Assets/Scripts/Dialogue/DialogueControl.cs
@@ -0,0 +1,78 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueControl : MonoBehaviour
+{
+    [System.Serializable]
+    public enum idiom
+    {
+        pt,
+        en,
+        es,
+    }
+
+    public idiom language;
+
+    [Header("Components")]
+    public GameObject dialogueObj;
+    public Image profileSprite;
+    public Text speechText;
+    public Text actorNameText;
+
+    [Header("Settings")]
+    public float typingSpeed;
+
+    private bool isShowing;
+    private int index;
+    private string[] sentences;
+
+    public static DialogueControl instance;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    IEnumerator TypeSentence()
+    {
+        foreach (char letter in sentences[index].ToCharArray())
+        {
+            speechText.text += letter;
+            yield return new WaitForSeconds(typingSpeed);   
+        }
+    }
+
+    public void NextSentence()
+    {
+        if (speechText.text == sentences[index])
+        {
+            if (index < sentences.Length - 1)
+            {
+                index++;
+                speechText.text = "";
+                StartCoroutine(TypeSentence());
+            }
+            else
+            {
+                speechText.text = "";
+                index = 0;
+                dialogueObj.SetActive(false);
+                sentences = null;
+                isShowing = false;
+            }
+        }
+    }
+
+    public void Speech(string[] txt)
+    {
+        if (!isShowing)
+        {
+            dialogueObj.SetActive(true);
+            sentences = txt;
+            StartCoroutine(TypeSentence());
+            isShowing = true;
+        }
+    }
+}
---
+++ Second Chance/Assets/Scripts/NPC/NPC_Dialogue.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPC_Dialogue : MonoBehaviour
+{
+    public float dialogueRange;
+    public LayerMask playerLayer;
+    public DialogueSettings dialogue;
+
+    private List<string> sentences = new List<string>();
+
+    bool playerHit;
+
+    private void Start()
+    {
+        GetNpcInfo();    
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E) && playerHit)
+        {
+            DialogueControl.instance.Speech(sentences.ToArray());
+        }
+    }
+
+    void GetNpcInfo()
+    {
+        for (int i = 0; i < dialogue.dialogues.Count; i++)
+        {
+            switch (DialogueControl.instance.language)
+            {
+                case DialogueControl.idiom.pt:
+                    sentences.Add(dialogue.dialogues[i].sentence.portuguese);
+                    break;
+                
+                case DialogueControl.idiom.en:
+                    sentences.Add(dialogue.dialogues[i].sentence.english);
+                    break;
+
+                case DialogueControl.idiom.es:
+                    sentences.Add(dialogue.dialogues[i].sentence.spanish);
+                    break;
+            }
+        }
+    }
+    void FixedUpdate()
+    {
+        ShowDialogue();
+    }
+
+    void ShowDialogue()
+    {
+        Collider2D hit = Physics2D.OverlapCircle(transform.position, dialogueRange, playerLayer);
+        if (hit != null)
+        {
+            playerHit = true;
+        }
+        else
+        {
+            playerHit = false;
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.DrawWireSphere(transform.position, dialogueRange);
+    }
+}
